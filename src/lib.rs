4bc64c9b9b0d237401d65f139fee200b4c0a0c03
Simon Berger <simon@siku2.io>
2020-09-18 17:19:15 +0200

    add loader hack

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
+// this macro is defined here so we can access it in the modules
+macro_rules! loader_hack {
+    ($ty:ty) => {
+        static LOADED: std::sync::Once = std::sync::Once::new();
+        impl $ty {
+            fn ensure_loaded() {
+                LOADED.call_once(|| {
+                    <$ty>::_dummy_loader();
+                });
+            }
+        }
+    };
+}
+
 mod button;
 pub use button::ButtonComponent;
-
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen(module = "/build/button.js")]
-extern "C" {
-    #[derive(Debug)]
-    type Button;
-}
