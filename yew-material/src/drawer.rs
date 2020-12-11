c8e3ec7a05b3a829a30055dc4f432683f2ea9b6d
Hamza <muhammadhamza1311@gmail.com>
2020-12-12 00:07:53 +0500

    add method to flip drawer state

--- yew-material/src/drawer.rs
+++ yew-material/src/drawer.rs
@@ -8,7 +8,7 @@ pub use drawer_header::MatDrawerHeader;
 pub use drawer_subtitle::MatDrawerSubtitle;
 pub use drawer_title::MatDrawerTitle;
 
-use crate::add_event_listener;
+use crate::{add_event_listener, WeakComponentLink};
 use wasm_bindgen::prelude::*;
 use web_sys::Node;
 use yew::prelude::*;
@@ -21,6 +21,9 @@ extern "C" {
 
     #[wasm_bindgen(getter, static_method_of = Drawer)]
     fn _dummy_loader() -> JsValue;
+
+    #[wasm_bindgen(method, getter)]
+    fn open(this: &Drawer) -> bool;
 
     #[wasm_bindgen(method, setter)]
     fn set_open(this: &Drawer, value: bool);
@@ -49,7 +52,7 @@ pub enum Msg {}
 ///
 /// - [Properties](https://github.com/material-components/material-components-web-components/tree/master/packages/drawer#propertiesattributes)
 /// - [Events](https://github.com/material-components/material-components-web-components/tree/master/packages/drawer#events)
-#[derive(Debug, Properties, Clone)]
+#[derive(Properties, Clone)]
 pub struct Props {
     #[prop_or_default]
     pub open: bool,
@@ -67,6 +70,8 @@ pub struct Props {
     /// See events docs to learn more.
     #[prop_or_default]
     pub onclosed: Callback<()>,
+    #[prop_or_default]
+    pub drawer_link: WeakComponentLink<MatDrawer>,
     pub children: Children,
 }
 
@@ -74,7 +79,8 @@ impl Component for MatDrawer {
     type Message = Msg;
     type Properties = Props;
 
-    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        props.drawer_link.borrow_mut().replace(link);
         Drawer::ensure_loaded();
         Self {
             props,
@@ -127,5 +133,17 @@ impl Component for MatDrawer {
                 &mut self.closed_closure,
             );
         }
+    }
+}
+
+impl WeakComponentLink<MatDrawer> {
+    /// A convenience method to for `drawer.open = !drawer.open`
+    pub fn flip_open_state(&self) {
+        let node_ref = (*self.borrow().as_ref().unwrap().get_component().unwrap())
+            .node_ref
+            .clone();
+        let element = node_ref.cast::<Drawer>().unwrap();
+        let open = element.open();
+        element.set_open(!open);
     }
 }
